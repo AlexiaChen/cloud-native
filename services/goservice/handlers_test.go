fe4117100d98c2e6a0ccfa06ce364873a8f6c5d5
MathxH Chen <brainfvck@foxmail.com>
2019-12-28 10:23:58 +0800

    Add TestCreateMatch

--- services/goservice/handlers_test.go
+++ services/goservice/handlers_test.go
@@ -4,8 +4,13 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/cloudnativego/gogo-engine"
 	"github.com/unrolled/render"
 )
 
@@ -14,6 +19,10 @@ var (
 		IndentJSON:    true,
 		IsDevelopment: true,
 	})
+)
+
+const (
+	fakeMatchLocationResult = "/matches/5a003b78-409e-4452-b456-a6f0dcee05bd"
 )
 
 func TestCreateMatchRespondsToBadData(t *testing.T) {
@@ -54,5 +63,86 @@ func TestCreateMatchRespondsToBadData(t *testing.T) {
 	defer res2.Body.Close()
 	if res2.StatusCode != http.StatusBadRequest {
 		t.Error("Sending valid JSON but with incorrect or missing fields should result in a bad request and didn't.")
+	}
+}
+
+func TestCreateMatch(t *testing.T) {
+	client := &http.Client{}
+	repo := newInMemoryRepository()
+	server := httptest.NewServer(http.HandlerFunc(createMatchHandler(formatter, repo)))
+	defer server.Close()
+
+	body := []byte("{\n  \"gridsize\": 19,\n  \"playerWhite\": \"alice\",\n  \"playerBlack\": \"alfred\"\n}")
+
+	req, err := http.NewRequest("POST", server.URL, bytes.NewBuffer(body))
+	if err != nil {
+		t.Errorf("Error in creating POST request for createMatchHandler: %v", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error in POST to createMatchHandler: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	payload, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error parsing response body: %v", err)
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		t.Errorf("Expected response status 201, received %s", res.Status)
+	}
+
+	loc, headerOk := res.Header["Location"]
+
+	if !headerOk {
+		t.Error("Location header is not set")
+	} else {
+		if !strings.Contains(loc[0], "/matches/") {
+			t.Errorf("Location header should contain '/matches/'")
+		}
+		if len(loc[0]) != len(fakeMatchLocationResult) {
+			t.Errorf("Location value does not contain guid of new match")
+		}
+	}
+
+	var matchResponse newMatchResponse
+	err = json.Unmarshal(payload, &matchResponse)
+	if err != nil {
+		t.Errorf("Could not unmarshal payload into newMatchResponse object")
+	}
+
+	if matchResponse.ID == "" || !strings.Contains(loc[0], matchResponse.ID) {
+		t.Error("matchResponse.Id does not match Location header")
+	}
+
+	// After creating a match, match repository should have 1 item in it.
+	matches, err := repo.getMatches()
+	if err != nil {
+		t.Errorf("Unexpected error in getMatches(): %s", err)
+	}
+	if len(matches) != 1 {
+		t.Errorf("Expected a match repo of 1 match, got size %d", len(matches))
+	}
+
+	var match gogo.Match
+	match = matches[0]
+	if match.GridSize != matchResponse.GridSize {
+		t.Errorf("Expected repo match and HTTP response gridsize to match. Got %d and %d", match.GridSize, matchResponse.GridSize)
+	}
+
+	if match.PlayerWhite != "alice" {
+		t.Errorf("Repository match, white player should be alice, got %s", match.PlayerWhite)
+	}
+
+	if matchResponse.PlayerWhite != "alice" {
+		t.Errorf("Expected white player to be alice, got %s", matchResponse.PlayerWhite)
+	}
+
+	if matchResponse.PlayerBlack != "alfred" {
+		t.Errorf("Expected black player to be alfred, got %s", matchResponse.PlayerBlack)
 	}
 }
